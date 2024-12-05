675a9564f84e710f5d0dff85b426b32416483df6
John Wallace <jdwallace12@gmail.com>
2024-12-05 00:16:49 -0500

    homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
     function ({ addUtilities }) {
       addUtilities({
         '.curved-hero': {
-          clipPath: 'ellipse(100% 100% at 50% 0%)', // Default for larger screens
+          clipPath: 'ellipse(100% 100% at 50% 0%)',
         },
       });
     },
